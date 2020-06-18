aec59f00ef750faff9cf9efe123fca26e9fe4c59
DeisData <66578709+DeisData@users.noreply.github.com>
2020-06-18 12:39:45 -0400

    Add files via upload

---
+++ python-working2.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 10:23:26 2020
+
+@author: Claire Pontbriand
+"""
+
+
+# # # # # # # # # # # # # # # #  
+#                             #
+# Built-in Functions and Help #
+#                             #
+# # # # # # # # # # # # # # # # 
+#
+#  How can I use built-in functions?
+#  How can I find out what they do?
+#  What kind of errors can occur in programs?
+
+
+#  Use comments to add documentation to programs.
+
+
+#  A FUNCTION may take zero or more arguments
+#  A function must always use parentheses
+
+
+
+# Commonly-used built-in functions include max, min, 
+#  and round.
+
+
+
+
+# Use the built-in function help to get help for a function.
+
+
+# Python reports syntax errors
+# when it can’t understand the source of a program.
+
+
+
+# Python reports a runtime error 
+# when something goes wrong while a program is executing.
+
+
+# # # # # # # # # # # # # # # #  
+#                             #
+# Libraries                   #
+#                             #
+# # # # # # # # # # # # # # # # 
+#
+#  How can I use software that other people have written?
+#
+
+
+# Most of the power of a programming language is in its libraries.
+# A library is a collection of files (called modules) 
+# that contains functions for use by other programs.
+# May also contain data values 
+# PyPI is the Python Package Index
+
+
+# A program must import a library module before using it.
+# Refer to things from the module as module_name.thing_name
+# Using math, one of the modules in the standard library:
+
+
+# Use help to learn about the contents of a library module.
+
+
+# Create an alias for a library module when importing 
+# it to shorten programs.
+
+
+
+
+
+# # # # # # # # # # # # # # # #  
+#                             #
+# Reading in Data with Pandas #
+#                             #
+# # # # # # # # # # # # # # # # 
+#
+# How can I read tabular data?
+#
+
+# Import the Pandas library.
+# Pandas is a widely-used Python library 
+# for statistics, particularly on tabular data.
+
+# Use Pandas to load a simple CSV data set.
+# Get some basic information about a Pandas DataFrame.
+# Use the Pandas library to do statistics on tabular data.
+
+
+
+# Use index_col to specify that a column's 
+# values should be used as row headings
+
+
+# Use DataFrame.info to find out more about a dataframe
+
+
+# Use DataFrame.T to transpose a dataframe
+
+
+# Use DataFrame.describe to get summary statistics 
+# about data.
+
+
+# Writing Data with to_csv
+
+
+# # # # #
+# Selecting Values in a Pandas DataFrame
+# # # # # 
+
+# What makes Pandas so attractive is the powerful interface 
+# to access individual records of the table, proper handling 
+# of missing values, and relational-databases operations 
+# between DataFrames.
+
+
+# Use DataFrame.iloc[..., ...] to select values by their (entry) position
+# Can specify location by numerical index 
+
+
+
+# Use DataFrame.loc[..., ...] to select values by their (entry) label.
+
+
+
+# Use : on its own to mean all columns or all rows.
+
+
+
+# # That's it for today!  On to the challenge questions and survey
+
+
+
+
+
